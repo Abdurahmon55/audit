feaba9f847389e6e4a3e4f8c8b0862d751ab50e1
Abdurahmon55 <116945590+Abdurahmon55@users.noreply.github.com>
2024-03-13 05:56:24 +0500

    created model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
+
+class GrupProduct(models.Model):
+    boss=models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='boss')
+    grupName=models.CharField(default='allProduict', max_length=100)
+
+    def __str__(self):
+        return self.grupName
+
+class Product(models.Model):
+    grup=models.ForeignKey(GrupProduct, on_delete=models.CASCADE, related_name='grup')
+    your_boolean_field = models.BooleanField(default=False)
+    productName=models.CharField(max_length=100)
+    priceProduct=models.IntegerField(blank=True)
+    totalProduct=models.IntegerField(blank=True)
+
+    def __str__(self):
+        return self.productName
+
+class Audit(models.Model):
+    boss=models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='audit')
+    totalMoney=models.IntegerField(blank=True)
+
+    def __str__(self):
+        return self.totalMoney
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     'corsheaders',
+    'api',
 ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
